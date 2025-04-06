055821a53958dcfdeeea9d11e9edd5c00e1a92d0
Christian <chirstiandave120702@gmail.com>
2025-04-06 09:52:40 +0800

    Minimal Updates

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Product\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::post('/create', [ProductController::class, 'CreateProduct'])->name('product.create');
 Route::get('/products', [ProductController::class, 'GetAllProducts'])->name('product.get');
 Route::put('/EditProduct/{id}', [ProductController::class, 'GetProductById'])->name('product.get.single');
-Route::delete('/DeleteProduct/{id}', [ProductController::class, 'DeleteProduct'])->name('product.delete');
+Route::delete('/ArchiveProduct/{id}', [ProductController::class, 'DeleteProduct'])->name('product.archive');
+Route::delete('/RestoreProduct/{id}', [ProductController::class, 'RestoreProduct'])->name('product.archive');
